b3b65b3f8819a19b533d72ec1d7ef76478895f73
ederson.mendes <ederson.mendes@lighthouseit.com.br>
2025-07-13 18:36:55 -0300

    controller cleanup and creation

---
+++ GameParser.API/Controllers/LogParserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameParser.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogParserController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok("OK");
+        }
+    }
+}
